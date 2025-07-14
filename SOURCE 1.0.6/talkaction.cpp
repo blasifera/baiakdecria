c995dc6f8f20d10b85d608b02e7065b6f693fc77
blasifera <lucasblasifera@hotmail.com>
2025-07-13 22:14:08 -0300

    s

--- SOURCE 1.0.6/talkaction.cpp
+++ SOURCE 1.0.6/talkaction.cpp
@@ -134,7 +134,8 @@ bool TalkActions::registerEvent(Event* event, xmlNodePtr p, bool override)
 
 bool TalkActions::onPlayerSay(Creature* creature, uint16_t channelId, const std::string& words, bool ignoreAccess, ProtocolGame* pg) //CA
 {
-	std::string cmd[TALKFILTER_LAST] = words, param[TALKFILTER_LAST] = "";
+	std::string cmd = words;
+	std::string param = "";
 	std::string::size_type loc = words.find('"', 0);
 	if(loc != std::string::npos)
 	{
